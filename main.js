f09a380eebcaf77f2585d5b2f69b3b1ea1dd7b97
OakAnderson <andersonfelipe01@live.com>
2021-03-17 09:08:49 -0300

    Exercise complete

--- main.js
+++ main.js
@@ -5,9 +5,26 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for ( var i = 1; i <= 5; i++ ) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/' + `pic${i}.jpg`);
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function (e) {
+        displayedImage.src = e.target.src;
+    }
+}
 
 /* Wiring up the Darken/Lighten button */
+
+btn.onclick = function (e) {
+    var className = btn.getAttribute('class');
+    if ( className == "dark" ) {
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+        overlay.style.background = "rgba(0, 0, 0, 0.5)";
+    } else {
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.background = "rgba(0, 0, 0, 0)";
+    }
+}
